456c18a191fe4c3edf548052ffcdc1bc4305f52b
DavidSantana01 <ds7991_@outlook.com>
2024-06-03 20:11:42 -0300

    Add get, post, put and delete

--- index.js
+++ index.js
@@ -1,9 +1,70 @@
 const express = require('express')
+const uuid = require('uuid')
 
+const port = 3000
 const app = express()
+app.use(express.json())
 
-app.get('/order', (request, response) => {
-    return response.send('We are running')
+const users = []
+
+app.get('/users', (request, response) => {
+    return response.json(users)
 })
 
-app.listen(3000)
+app.post('/users', (request, response) => {
+    const { order, clientName, price, status } = request.body
+    
+    const user = {id:uuid.v4(),  order, clientName, price, status }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+})
+
+app.put('/users/:id', (request, response) => {
+    const { id } = request.params
+    const { order, clientName, price, status } = request.body
+
+    const updatedUser = {id, order, clientName, price, status }
+
+    const index = users.findIndex(user => user.id === id)
+    
+    if(index <0){
+        return response.status(404).json({ message: "user not found"})
+    }
+
+    users[index] = updatedUser
+
+    return response.json(updatedUser)
+})
+
+app.delete('/users/:id', (request, response) => {
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if(index <0){
+        return response.status(404).json({ message: "user not found"})
+    }
+
+    users.splice(index,1)
+
+    return response.status(204).json({ message:"Pedido deletado com sucesso"})
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`🚀Server started on port ${port}`)
+})
+
+
+/*
+{
+	"order": "X- Salada, 2 batatas grandes, 1 coca-cola",
+	"clientName":"José",
+	"price": 44.50
+}
+
+*/
